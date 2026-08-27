b2d001e7f348f8a8c46a6c954f181b298abbcde3
teffun <teffun@example.com>
2026-08-28 01:01:07 +0300

    Fix missing statusBarsPadding import in MainActivity

--- app/src/main/java/com/ir/tester/MainActivity.kt
+++ app/src/main/java/com/ir/tester/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.GraphicEq
 import androidx.compose.material.icons.filled.Info
